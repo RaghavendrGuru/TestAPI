1ecc8a3a1d54dd207fe388a8a8c9bddaaea93776
RaghavendrGuru <55948587+RaghavendrGuru@users.noreply.github.com>
2025-05-29 15:42:48 +0530

    Add files via upload

--- src/main/java/com/api/controller/TestAPIController.java
+++ src/main/java/com/api/controller/TestAPIController.java
@@ -3,7 +3,6 @@ package com.api.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/webhook")
-@CrossOrigin(origins = "*", allowedHeaders = "*") 
 public class TestAPIController {
 	
 	//http://localhost:8085/testapi/user/
